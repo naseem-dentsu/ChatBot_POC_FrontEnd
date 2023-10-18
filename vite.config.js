ec67d41d588d3e28775ff05fc2afca7673305bf0
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-10-18 18:41:07 +0530

    changed config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://naseem-dentsu.github.io/ChatBot_POC_FrontEnd"
 })
