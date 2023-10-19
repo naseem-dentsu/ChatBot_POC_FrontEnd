40d4eff97a7b08f388bd9eb9ec035b654e074def
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-10-19 14:58:53 +0530

    Added production url

--- src/context/message.context.jsx
+++ src/context/message.context.jsx
@@ -7,6 +7,10 @@ export const MessageProvider = ({ children }) => {
     const [history, setHistory] = useState("");
     const [ansMessage, setAnsMessage] = useState("");
     const [chatThread, setChatThread] = useState([])
+
+    const { PROD, VITE_APP_BASE_URL, VITE_APP_API_BASE_PROD_URL } = import.meta.env;
+
+
     const fireQuery = (queryMessage) => {
         console.log("query", queryMessage);
         if (queryMessage) {
@@ -16,7 +20,7 @@ export const MessageProvider = ({ children }) => {
             }
             axios.
                 post(
-                    import.meta.env.VITE_APP_BASE_URL + "query/search",
+                    PROD ? VITE_APP_API_BASE_PROD_URL : VITE_APP_BASE_URL + "query/search",
                     param,
                     // { baseURL: import.meta.env.VITE_APP_BASE_URL }
                     { headers: { "content-type": "application/json" } }
