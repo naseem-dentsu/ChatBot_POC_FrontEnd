00e12c2895b51f7ad69091cbbbd9e9c9369ee0ee
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-10-31 16:36:50 +0530

    Added Microphone stuff to app

--- src/component/inputArea.component.jsx
+++ src/component/inputArea.component.jsx
@@ -1,10 +1,22 @@
+import 'regenerator-runtime';
 import { useContext, useEffect } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import { MessageContext } from '../context/message.context'
 import sentIcon from '../assets/sent.svg'
-import micIcon from '../assets/mic.svg'
+import micIconClose from '../assets/mic.svg'
+import micIconOpen from '../assets/voice-search.png'
+
 
 function InputArea() {
-    const { setInputDisabled, setLoading, fireQuery, loading } = useContext(MessageContext)
+    const { setInputDisabled, setLoading, fireQuery, loading } = useContext(MessageContext);
+
+    const {
+        transcript,
+        listening,
+        browserSupportsSpeechRecognition,
+        isMicrophoneAvailable,
+    } = useSpeechRecognition();
+
     onchange = () => {
         const element = document.getElementById("chat-input")
         if (element.value.length !== 0) {
@@ -15,8 +27,30 @@ function InputArea() {
         }
     }
     const onMicClick = () => {
-        console.log("called")
+        if (!browserSupportsSpeechRecognition) {
+            return console.error('Browser doesn\'t support speech recognition.');
+        }
+        if (!isMicrophoneAvailable) {
+            alert("Please Enable Microphone Permissions to use mic");
+            return
+        }
+        if (!listening) {
+            SpeechRecognition.startListening()
+        }
+        else {
+            SpeechRecognition.stopListening()
+        }
     }
+
+    useEffect(() => {
+        if (listening) {
+            const element = document.getElementById("chat-input")
+            if (element) {
+                element.value = transcript;
+            }
+        }
+    }, [listening, transcript])
+
     useEffect(() => {
         const element = document.getElementById("chat-input")
         if (element) {
@@ -29,7 +63,7 @@ function InputArea() {
             <div className='input-component-inter'>
                 <input id="chat-input" className='input-text' type='text' placeholder='Write to Bot' onSubmit={onchange} />
                 <div className='mic-icon-div'>
-                    <img src={micIcon} className="mic-icon" alt="Mic logo" onClick={onMicClick} />
+                    <img src={listening ? micIconOpen : micIconClose} className="mic-icon" alt="Mic logo" onClick={onMicClick} />
                 </div>
             </div>
             <div className='sent-icon-div'>
