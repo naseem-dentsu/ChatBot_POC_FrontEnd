e9205ebc709e26b1a99a7cf36bea904df0a85a0b
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-12-01 18:03:05 +0530

    Added image parsing in parser

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -8,16 +8,21 @@ export const scrollToBottom = () => {
   }
 }
 export const responseParser = (data) => {
-  const pluckingregex = /\(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)\)/gm
+  const pluckingregex = /[\[|\(]https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)[\)|\]]/gm
   const here = /\[here\]/gm
   const url = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm
+  const imgurl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)\.jpg|\.png/gm
   const newline = /(?:\r\n|\r|\n)/g
 
   data = data.replaceAll(newline, ' <br>');
   data = data.replaceAll(here, "")
   data = data.replace(pluckingregex, function (matched) {
-
-    return ` <br/> <a href=${matched.match(url)[0]} target="_blank" classname="hyperlink"> Go to Page </a>`
+    if (matched.match(imgurl)) {
+      return ` <br/> <img src=${matched.match(url)[0]} class="product-image"/>`
+    }
+    else {
+      return ` <br/> <a href=${matched.match(url)[0]} target="_blank" class="hyperlink"> Go to Page </a>`
+    }
   })
 
   return data;
