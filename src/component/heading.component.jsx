d38db15814ad59730ce0cd231093435dfe78f36c
indranil-k <indranil.karmarkar@merklecxm.com>
2023-12-01 12:45:10 +0530

    Changed name

--- src/component/heading.component.jsx
+++ src/component/heading.component.jsx
@@ -5,7 +5,7 @@ function Heading() {
             <header>
                 <img src="https://shiseido.ipscdn.net/sa1/on/demandware.static/-/Sites-shiseido_us-Library/default/dw0adc8875/sfsc/Shiseido.Logotype.Negative.2020.png"/>
                 <h2 className='heading-text'>
-                    Merkle Chat Bot
+                    Virtual Assistant
                 </h2>
             </header>
         </div>
