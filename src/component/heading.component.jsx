38c374fe143cad257e4927a5fb0d0f84ac2fea35
indranil-k <indranil.karmarkar@merklecxm.com>
2023-12-01 11:55:58 +0530

    FE changes

--- src/component/heading.component.jsx
+++ src/component/heading.component.jsx
@@ -2,9 +2,12 @@ function Heading() {
 
     return (
         <div className='heading'>
-            <p className='heading-text'>
-                Merkle Chat Bot
-            </p>
+            <header>
+                <img src="https://shiseido.ipscdn.net/sa1/on/demandware.static/-/Sites-shiseido_us-Library/default/dw0adc8875/sfsc/Shiseido.Logotype.Negative.2020.png"/>
+                <h2 className='heading-text'>
+                    Merkle Chat Bot
+                </h2>
+            </header>
         </div>
 
 
