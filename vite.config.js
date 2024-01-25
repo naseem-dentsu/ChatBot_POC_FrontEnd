95d069d3cc8016ef086a6d855ed4cd4d66b4d02f
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2024-01-25 16:24:41 +0530

    Changed prot to 4000

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://naseem-dentsu.github.io/ChatBot_POC_FrontEnd"
+  base: "https://naseem-dentsu.github.io/ChatBot_POC_FrontEnd",
+  server: {
+    port: 4000
+  }
 })
